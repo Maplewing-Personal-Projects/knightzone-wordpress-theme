cd98b25048c64e7ce8058bc87f0f1748325c5944
Maplewing <sinmaplewing@gmail.com>
2019-12-05 09:52:40 +0800

    Add outer-container

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php get_header(); ?>
-<div class="container">
-<?php
-  if(have_posts()):
-    while(have_posts()): the_post(); ?>
-      <article class="post">
-        <h1 class="post-title"><?php the_title() ?></h1>
-        <?php the_content() ?>
-      </article>
-  <?php 
-    endwhile;
-  else:
-    echo '<p>There are no posts!</p>';
-  endif;
-?>
+<div class="outer-container">
+  <div class="container">
+  <?php
+    if(have_posts()):
+      while(have_posts()): the_post(); ?>
+        <article class="post">
+          <h1 class="post-title"><?php the_title() ?></h1>
+          <?php the_content() ?>
+        </article>
+    <?php 
+      endwhile;
+    else:
+      echo '<p>There are no posts!</p>';
+    endif;
+  ?>
+  </div>
 </div>
 <?php get_footer(); ?>
