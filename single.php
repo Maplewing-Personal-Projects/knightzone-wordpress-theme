6edef41a195d69119aa1b61681d37796794c0095
Maplewing <sinmaplewing@gmail.com>
2019-12-07 17:45:35 +0800

    Adjust the background order

--- single.php
+++ single.php
@@ -10,7 +10,7 @@
       if($have_post):
     ?>
         <div class="post-header" 
-          style="background: <?php if(has_post_thumbnail()): ?> url('<?php the_post_thumbnail_url(); ?>') <?php endif; ?> rgba(0, 0, 0, 0.6); background-size: cover; background-position: center center;">
+          style="background:  rgba(0, 0, 0, 0.6), <?php if(has_post_thumbnail()): ?> url('<?php the_post_thumbnail_url(); ?>') <?php endif; ?>; background-size: cover; background-position: center center;">
             <h1 class="post-title"><?php the_title() ?></h1>
         </div>
     <?php
