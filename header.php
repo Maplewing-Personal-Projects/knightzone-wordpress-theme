318d83fe9f57dd542229a8ce2b6a6ff14995f9a4
Maplewing <sinmaplewing@gmail.com>
2019-12-07 19:12:35 +0800

    Directly include javascript

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
             h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
         })(document);
     </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="<?php echo get_template_directory_uri() . '/scripts/parallax.js-1.5.0/parallax.min.js'; ?>"></script>
     <?php wp_head() ?>
 </head>
  
