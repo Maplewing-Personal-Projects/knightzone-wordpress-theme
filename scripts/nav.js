de9d8996f78beb1079b9df23b3bb3ce885bce1c0
Maplewing <sinmaplewing@gmail.com>
2019-12-08 18:39:04 +0800

    Fix typo

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(){
     var openClassName = "open";
 
     document.querySelector("#nav-open-button").addEventListener("click", function(){
-        var navSection = document.querySelector("#navSection");
+        var navSection = document.querySelector("#nav-section");
         if (navSection.classList.contains(openClassName))
             navSection.classList.remove(openClassName);
         else
