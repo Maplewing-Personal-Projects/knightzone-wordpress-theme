393f9c0831ddba5660ee9961638ff60457ddebaf
Maplewing <sinmaplewing@gmail.com>
2019-12-08 22:18:11 +0800

    Add some content in home.php

--- home.php
+++ home.php
@@ -11,6 +11,20 @@
     </div>
     <article class="post">
         <?php
+            $home_page = get_post(3667);
+            $content = home_page->post_content;
+            $content = apply_filters('the_content', $content);
+            $content = str_replace(']]>', ']]>', $content);
+            echo $content;
+        ?>
+        <h2>分類瀏覽</h2>
+        <div class="list-total-categories">
+        <?php
+            wp_list_categories();
+        ?>
+        </div>
+        <h2>最新文章</h2>
+        <?php
         if(have_posts()):
         ?>
             <ul class="post-item-list">
@@ -57,6 +71,16 @@
         endif;
         ?>
         </div>
+        <h2>最新回應</h2>
+        <ul>
+            <?php $args_comments = array('number' => 10, 'status' => 'approve');
+            $recent_comments = get_comments($args_comments);
+            foreach ($recent_comments as $r) {
+                echo '<li><a href="'. get_comment_link($r->comment_ID) .'" title="'. __('Date/time:', 'shapespace') . $r->comment_date .'">';
+                echo $r->comment_author .'</a>: ';
+                echo shapeSpace_truncate_string(get_comment_excerpt($r->comment_ID), 12) .'</li>';
+            } ?>
+        </ul>
     </article>
   </div>
 </div>
