a348d987a20542d4cf429d9305f6c214772a4f2a
Maplewing <sinmaplewing@gmail.com>
2019-12-08 23:38:17 +0800

    Fix compile error

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,38 +1,44 @@
-var openClassName = "open";
-var goToTopButton = document.getElementById("go_to_top_button");
+window.addEventListener("load", function(){
+    var openClassName = "open";
+    var goToTopButton = document.getElementById("go_to_top_button");
 
-/* Code from https://stackoverflow.com/questions/21474678/scrolltop-animation-without-jquery */
-function scrollToTop(scrollDuration) {
-    var cosParameter = window.scrollY / 2,
-        scrollCount = 0,
-        oldTimestamp = performance.now();
-    function step (newTimestamp) {
-        scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
-        if (scrollCount >= Math.PI) window.scrollTo(0, 0);
-        if (window.scrollY === 0) return;
-        window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
-        oldTimestamp = newTimestamp;
+    /* Code from https://stackoverflow.com/questions/21474678/scrolltop-animation-without-jquery */
+    function scrollToTop(scrollDuration) {
+        var cosParameter = window.scrollY / 2,
+            scrollCount = 0,
+            oldTimestamp = performance.now();
+        function step (newTimestamp) {
+            scrollCount += Math.PI / (scrollDuration / (newTimestamp - oldTimestamp));
+            if (scrollCount >= Math.PI) window.scrollTo(0, 0);
+            if (window.scrollY === 0) return;
+            window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
+            oldTimestamp = newTimestamp;
+            window.requestAnimationFrame(step);
+        }
         window.requestAnimationFrame(step);
     }
-    window.requestAnimationFrame(step);
-}
 
-window.addEventListener("scroll", function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        goToTopButton.style.display = "block";
-    } else {
-        goToTopButton.style.display = "none";
+    function displayGoToTopButton() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopButton.style.display = "block";
+        } else {
+            goToTopButton.style.display = "none";
+        }
     }
-});
 
-document.querySelector("#nav-open-button").addEventListener("click", function(){
-    var navSection = document.querySelector("#nav-section");
-    if (navSection.classList.contains(openClassName))
-        navSection.classList.remove(openClassName);
-    else
-        navSection.classList.add(openClassName);
-});
+    window.addEventListener("scroll", displayGoToTopButton);
 
-goToTopButton.addEventListener("click", function(){
-    scrollToTop(1);
+    document.querySelector("#nav-open-button").addEventListener("click", function(){
+        var navSection = document.querySelector("#nav-section");
+        if (navSection.classList.contains(openClassName))
+            navSection.classList.remove(openClassName);
+        else
+            navSection.classList.add(openClassName);
+    });
+
+    goToTopButton.addEventListener("click", function(){
+        scrollToTop(1);
+    });
+
+    displayGoToTopButton();
 })
