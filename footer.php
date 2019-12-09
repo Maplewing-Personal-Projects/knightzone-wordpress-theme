e7ffaed60785b16dc18f97fd8fd449f183a2c301
Maplewing <sinmaplewing@gmail.com>
2019-12-09 09:47:21 +0800

    Adjust the footer structure

--- footer.php
+++ footer.php
@@ -1,27 +1,29 @@
 <footer class="site-footer">
-    <div class="category-navigator">
-        <h2>分類瀏覽</h2>
-        <div class="list-total-categories">
-        <?php
-            wp_list_categories(array("title_li" => "全部分類"));
-        ?>
-        </div>
-    </div>
-    <div class="tag-navigator">
-        <h2>標籤瀏覽</h2>
-        <div class="list-total-tags">
-        <?php
-            the_all_tags();
-        ?>
-        </div>
-    </div>
     <div class="footer-container">
-        <section class="copyright"><?php bloginfo('name') ?> ( <?php bloginfo('url') ?> ) By 灆洢 ( Maplewing )</section>
-        <nav>
-            <?php wp_nav_menu( array( 
-            'theme_location' => 'extra-menu',
-            'menu_class' => 'extra-menu' ) ); ?>
-        </nav>
+        <div class="category-navigator">
+            <h2>分類瀏覽</h2>
+            <div class="list-total-categories">
+            <?php
+                wp_list_categories(array("title_li" => "全部分類"));
+            ?>
+            </div>
+        </div>
+        <div class="tag-navigator">
+            <h2>標籤瀏覽</h2>
+            <div class="list-total-tags">
+            <?php
+                the_all_tags();
+            ?>
+            </div>
+        </div>
+        <div class="copyright-wrapper">
+            <section class="copyright"><?php bloginfo('name') ?> ( <?php bloginfo('url') ?> ) By 灆洢 ( Maplewing )</section>
+            <nav>
+                <?php wp_nav_menu( array( 
+                'theme_location' => 'extra-menu',
+                'menu_class' => 'extra-menu' ) ); ?>
+            </nav>
+        </div>
     </div>
 </footer>
  
