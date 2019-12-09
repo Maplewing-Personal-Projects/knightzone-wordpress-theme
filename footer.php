71fd0c83685fdd4ffd367c94d832092c9d29fffe
Maplewing <sinmaplewing@gmail.com>
2019-12-09 09:35:24 +0800

    Adjust some style & add categories/tags to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
 <footer class="site-footer">
+    <h2>分類瀏覽</h2>
+        <div class="list-total-categories">
+        <?php
+            wp_list_categories(array("title_li" => "全部分類"));
+        ?>
+    </div>
+    <h2>標籤瀏覽</h2>
+    <div class="list-total-tags">
+    <?php
+        the_all_tags();
+    ?>
     <div class="footer-container">
         <section class="copyright"><?php bloginfo('name') ?> ( <?php bloginfo('url') ?> ) By 灆洢 ( Maplewing )</section>
         <nav>
